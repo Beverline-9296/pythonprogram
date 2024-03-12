6ccbb28270d31e5c58c515da0f991449a7f0de94
Beverline-9296 <144214962+Beverline-9296@users.noreply.github.com>
2024-03-12 11:34:27 +0300

    point of sale

--- point_of_sale.py
+++ point_of_sale.py
@@ -41,4 +41,43 @@ class ServiceItem(SaleItem):
         total_cost = self.hourly_rate * hours
         return total_cost
 
-https://github.com/Beverline-9296/pythonprogram.git
+
+# Input sale details
+item_id = input("Enter the item ID: ")
+name = input("Enter the item name: ")
+unit_price = float(input("Enter the unit price: "))
+quantity = int(input("Enter the quantity: "))
+
+# Loop until the user chooses to exit
+while True:
+    print("Choose the product type:")
+    print("1. Standard")
+    print("2. Discounted")
+    print("3. Service")
+    print("4. Exit")
+    item_type = int(input("Enter your choice (1/2/3/4): "))
+
+    if item_type == 4:
+        print("Exiting the program.")
+        break
+
+    # Create an instance of the corresponding item type
+    if item_type == 1:
+        item = StandardItem(item_id, name, unit_price)
+        total_cost = item.calculate_total(quantity)
+    elif item_type == 2:
+        discount_percentage = float(input("Enter the discount percentage: "))
+        item = DiscountedItem(item_id, name, unit_price, discount_percentage)
+        total_cost = item.calculate_total(quantity)
+    elif item_type == 3:
+        hourly_rate = float(input("Enter the hourly rate: "))
+        item = ServiceItem(item_id, name, hourly_rate)
+        hours = int(input("Enter the number of hours: "))
+        total_cost = item.calculate_total(hours)
+    else:
+        print("Invalid item type.")
+        continue
+
+    # Print total cost
+    print("Total cost:", total_cost)
+
